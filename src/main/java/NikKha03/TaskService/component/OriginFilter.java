87b73f40c6917f0f733efa63190886133d399a0e
NikKha03 <kolyakhalimendik@gmail.com>
2025-11-16 01:40:34 +0300

    Добавил gatewaySecret

--- src/main/java/NikKha03/TaskService/component/OriginFilter.java
+++ src/main/java/NikKha03/TaskService/component/OriginFilter.java
@@ -2,31 +2,30 @@ package NikKha03.TaskService.component;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
-import java.util.List;
 
 @Component
 public class OriginFilter implements Filter {
 
-    private static final Logger logger = LoggerFactory.getLogger(OriginFilter.class);
-
-    @Value("#{'${app-env.allowed-origins}'.split(',')}")
-    private List<String> ALLOWED_ORIGIN;
+    @Value("${app-env.gateway-secret}")
+    private String gatewaySecret;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String origin = httpRequest.getScheme() + "://" + httpRequest.getHeader("Host");
 
-        if (!ALLOWED_ORIGIN.contains(origin)) {
-            logger.warn("Нераспознанный origin: " + origin);
+        if (!gatewaySecret.equals(httpRequest.getHeader("X-Gateway-Secret"))) {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            resp.getWriter().write("Access denied: missing or invalid Gateway header");
+            return;
         }
+
         chain.doFilter(request, response); // Пропускаем запрос
     }
 
